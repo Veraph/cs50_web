5514f9a8ab5e6c0e0c3056ff4cc18b0a7be84888
Mengwei <jmw3531@live.com>
2020-06-13 16:37:25 +0100

    Use Java Script to let our webpage be more interactive

---
+++ js/intro.js
@@ -0,0 +1,14 @@
+// build more interactive website
+// almost identical with C, but is a language that browser can interpret and run
+// Document Obejct Model(DOM), JS can modify DOM structure
+
+
+// do not need to specify the type of the variable, use let introduce new variables
+let counter = 0;
+
+// function
+function cough(n)
+{
+    print(cough) * n
+}
+
